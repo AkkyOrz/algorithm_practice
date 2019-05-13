44d11cd498a5517d2a2b781f854610fb69b226af
A <exam@sample.com>
2019-05-13 12:49:36 +0900

    hoge
    template作成した

---
+++ practice/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+#define int long long
+
+typedef long long ll;
+
+signed main(){
+    return 0;
+}
