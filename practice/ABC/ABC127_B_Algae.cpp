42388f727906ef6b66ac66feae747b9bbf6f31e1
A <exam@sample.com>
2019-06-04 05:33:40 +0900

    hoge

---
+++ practice/ABC/ABC127_B_Algae.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+// typedef
+//------------------------------------------
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+// rep
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define ALL(x) (x).begin(), (x).end()
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+
+
+int r, d, x;
+signed main()
+{
+    cin >> r >> d >> x;
+
+    int res = r;
+    FOR(i, 1, 11){    
+        x = r*x - d;
+        cout << x << endl;
+    }
+}
---
+++ practice/ABC/ABC127_E_CellDistance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+// typedef
+//------------------------------------------
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+// rep
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define ALL(x) (x).begin(), (x).end()
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+
+const int MOD = 1e9 + 7;
+
+
+int n, m, k;
+
+
+signed main()
+{
+    cin >> n >> m >> k;
+}
