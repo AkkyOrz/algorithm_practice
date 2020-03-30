399b7219fecb3ebb08df41a3aa870279d2d89fbe
Akihide Ito <akky.supernova@gmail.com>
2020-03-30 09:41:11 +0900

    17 and 18 is not solved

---
+++ practice/ABC/ABC124_A_Buttons.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+// typedef
+//------------------------------------------
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+// rep
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define ALL(x) (x).begin(), (x).end()
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+#define DEBUGP(x) cerr << #x << ": " << x.first << " \t" << x.second << '\n'
+
+int a, b;
+signed main()
+{
+    int sum = 0;
+    cin >> a >> b;
+    if (a < b) {
+        sum += b;
+        b--;
+    } else {
+        sum += a;
+        a--;
+    }
+
+    cout << sum + max(a, b) << endl;
+    return 0;
+}
