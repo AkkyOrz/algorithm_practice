b955af91fcce21e4a963d2a8f122180645af9428
Akihide Ito <akky.supernova@gmail.com>
2020-04-02 19:18:42 +0900

    daily commit

---
+++ practice/ABC/ABC159_A_TheNumberofEvenPairs.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+// typedef
+//------------------------------------------
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+// rep
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define ALL(x) (x).begin(), (x).end()
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+#define DEBUGP(x) cerr << #x << ": " << x.first << " \t" << x.second << '\n'
+
+int n, m;
+
+signed main()
+{
+    cin >> n >> m;
+
+    cout << n * (n - 1) / 2 + m * (m - 1) / 2 << endl;
+    return 0;
+}
